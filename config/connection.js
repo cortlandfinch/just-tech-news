f3829938de9bf6499a411123f44d375f36f2d06e
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-28 22:35:55 -0400

    update files with end of 13.5

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.DB_PASSWORD, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
-    user: 'root',
-    password: 'Stammer#2',
-    port: '3306',
-    database: 'just_tech_news_db',
-    dialect: 'mysql'
-});
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
-const sequelize = require('sequelize');
-const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-  console.log('==============');
+  console.log('======================');
   Post.findAll({
-    // update the `.findAll()` method's attributes to look like this
     attributes: [
       'id',
       'post_url',
@@ -16,6 +15,14 @@ router.get('/', (req, res) => {
     ],
     order: [['created_at', 'DESC']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -41,17 +48,18 @@ router.get('/:id', (req, res) => {
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    // replace the existing `include` with this
     include: [
       {
-        model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
       },
       {
-        model: Post,
-        attributes: ['title'],
-        through: Vote,
-        as: 'voted_posts'
+        model: User,
+        attributes: ['username']
       }
     ]
   })
@@ -82,14 +90,13 @@ router.post('/', (req, res) => {
     });
 });
 
-// PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(500).json(err);
     });
 });
 
