d2ddef7b8d94ff3c2ce03ff321ed763df84d176a
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-09-02 11:08:01 -0400

    update file structure and files with seeds

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,16 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Stammer#2',
+    port: '3306',
+    database: 'just_tech_news_db',
+    dialect: 'mysql'
   });
-}
 
 module.exports = sequelize;
