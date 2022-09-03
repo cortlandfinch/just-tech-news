9dad2d302a9e9b2ae3af04a01503bd0679965051
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-09-03 16:57:14 -0400

    add login and logout functionality

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUnintialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
  
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -17,6 +31,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
