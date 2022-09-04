c264adeb7a8294b0a04dcb87c859fe81c761c00e
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-09-04 16:27:11 -0400

    it works

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,14 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
 // create connection to our db
-// create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Stammer#2',
-    port: '3306',
-    database: 'just_tech_news_db',
-    dialect: 'mysql'
-  });
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
